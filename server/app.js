38d8072262ff5445e79302a68c3b0b5fbcc64476
Mikha38 <oyks1379@yandex.ru>
2022-11-03 14:02:14 +0300

    clear

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+});
+app.use(express.json())
+app.get('/', (req, res) => {
+    return res.send('ara')
+})
+const start = async () => {
+    try{
+        app.listen(4000, ()=>{
+            console.log(`Сервер ожидает запросов на порте ${4000}`)
+        })
+    }catch(e){
+        console.error(e)
+    }
+}
+start()
